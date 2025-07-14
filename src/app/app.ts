fae98a45ee9ece9516a353aaacc35a9e23a8b4eb
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-14 21:31:11 +0300

    done with project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lorem, random } from 'faker';
+import { of } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('typing');
+
+  randomText = lorem.sentence();
+  isHidden=false;
+  userTyping="";
+
+  onInput(event:Event):void {
+  const input = event.target as HTMLInputElement;
+  const value = input.value;
+
+  this.userTyping=value;
+
+
+    if(this.randomText==value)
+      this.isHidden=true;
+    else
+      this.isHidden=false;
+  } 
+
+
+  letterColor(letter:string,i:number):string{
+
+    
+    if(this.userTyping[i]===letter)
+      return "#25FF29";
+    else if(this.userTyping[i]!==letter && this.userTyping.length>i)
+      return "#FF0000";
+    else
+      return "gray";
+  }
+
+  reloadPage() {
+  window.location.reload();
+}
+
+
 }
